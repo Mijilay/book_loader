29a0cc476fc64bf931c357caeba2e9ff98a2b9f9
Mijilay <113235115+Mijilay@users.noreply.github.com>
2024-07-10 19:40:54 +0300

    download books from tululu.org

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
-import os
+
 for number in range(1,10):
     url = f'https://tululu.org/txt.php?id={number}'
-    book = requests.get(url)
-    with open (f"book_id{number}.txt", 'wb') as file:
-        file.write(book.content)
+    response = requests.get(url)
+    response.raise_for_status()
+    with open (f"/books/book_id{number}.txt", 'wb') as file:
+        file.write(response.content)
