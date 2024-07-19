4bde9954fbcb6f5894adfd832e0f1ed265278c65
Mijilay <113235115+Mijilay@users.noreply.github.com>
2024-07-19 10:52:42 +0300

    download book covers

--- main.py
+++ main.py
@@ -2,17 +2,28 @@ import requests
 import os
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
+from urllib.parse import urljoin
+from urllib.parse import urlsplit, unquote
 
 
 def check_for_redirect(response):
     if response.history:
         raise requests.exceptions.HTTPError
 
-def save_txt(url, filename, folder='books/'):
+def save_book(url, filename, folder='books/'):
     os.makedirs('books', exist_ok=True)
     response = requests.get(url)
     response.raise_for_status()
     filepath = os.path.join(f'{folder}{sanitize_filename(filename)}.txt')
+    with open (filepath, 'wb') as file:
+        file.write(response.content)
+
+def save_pic(url, filename, folder='covers/'):
+    os.makedirs('covers', exist_ok=True)
+    response = requests.get(url)
+    response.raise_for_status()
+    filepath = os.path.join(f'{folder}{sanitize_filename(filename)}')
+    print(filepath)
     with open (filepath, 'wb') as file:
         file.write(response.content)
 
@@ -26,12 +37,28 @@ for number in range(1,11):
         response_url_read = requests.get(url_read)
         response_url_read.raise_for_status()
         soup = BeautifulSoup(response_url_read.text, 'lxml')
+        pic_tag = soup.find('div', class_='bookimage').find('img')['src']
+        full_image_url = urljoin(url_read, pic_tag)
+        #print(pic_tag)
+        #print(full_image_url)
+        #pic_tag.
+        image_name_ext = (pic_tag.split('/'))[2]
+        # imagename,ext = (image_name_ext.split('/'))
+        splited_url = urlsplit(full_image_url)
+        scheme, netloc, path, query, fragment = (splited_url)
+        print(splited_url)
+        #print(imagename, ext)
         title_tag = soup.find('h1').text
-        print(title_tag)
         title,author = title_tag.split('::')
-        filename = f'{number}.{title.strip()}'
-        save_txt(url_text, filename)
-    
+        #adress,name = pic_tag.strip()
+        filename_book = f'{number}.{title.strip()}'
+        filename = urlsplit(full_image_url).path.split('/')[-1]
+        print(filename)
+        #print(filename)
+        #filename_cover = f'{imagename}.{title.strip()}.{ext}'
+        save_book(url_text, filename_book)
+        save_pic(full_image_url, filename)
+
         
     except requests.exceptions.HTTPError:
         print('Страница не существует')
