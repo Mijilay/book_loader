fb07c3c6de34f47d18a4ab2593eb37f9bf71d747
Mijilay <113235115+Mijilay@users.noreply.github.com>
2024-07-23 10:09:00 +0300

    download book covers

--- main.py
+++ main.py
@@ -30,35 +30,53 @@ def save_pic(url, filename, folder='covers/'):
 for number in range(1,11):
     try:  
         url_text = f'https://tululu.org/txt.php?id={number}'
-        response_url_text = requests.get(url_text)
-        response_url_text.raise_for_status()
-        check_for_redirect(response_url_text)
         url_read= f'https://tululu.org/b{number}'
         response_url_read = requests.get(url_read)
         response_url_read.raise_for_status()
+        response_url_text = requests.get(url_text)
+        response_url_text.raise_for_status()
+        check_for_redirect(response_url_text)
         soup = BeautifulSoup(response_url_read.text, 'lxml')
-        pic_tag = soup.find('div', class_='bookimage').find('img')['src']
-        full_image_url = urljoin(url_read, pic_tag)
         #print(pic_tag)
         #print(full_image_url)
         #pic_tag.
-        image_name_ext = (pic_tag.split('/'))[2]
-        # imagename,ext = (image_name_ext.split('/'))
-        splited_url = urlsplit(full_image_url)
-        scheme, netloc, path, query, fragment = (splited_url)
-        print(splited_url)
         #print(imagename, ext)
         title_tag = soup.find('h1').text
         title,author = title_tag.split('::')
         #adress,name = pic_tag.strip()
         filename_book = f'{number}.{title.strip()}'
+        save_book(url_text, filename_book)
+        pic_tag = soup.find('div', class_='bookimage').find('img')['src']
+        full_image_url = urljoin(url_read, pic_tag)
+        image_name_ext = (pic_tag.split('/'))[2]
+        # imagename,ext = (image_name_ext.split('/'))
+        splited_url = urlsplit(full_image_url)
+        scheme, netloc, path, query, fragment = (splited_url)
+        print(splited_url)
         filename = urlsplit(full_image_url).path.split('/')[-1]
         print(filename)
-        #print(filename)
-        #filename_cover = f'{imagename}.{title.strip()}.{ext}'
-        save_book(url_text, filename_book)
         save_pic(full_image_url, filename)
+        
 
         
     except requests.exceptions.HTTPError:
         print('Страница не существует')
+# for number in range(1,11):
+#     try:
+#         url_read= f'https://tululu.org/b{number}'
+#         response_url_read = requests.get(url_read)
+#         response_url_read.raise_for_status()
+#         check_for_redirect(response_url_text)
+#         soup = BeautifulSoup(response_url_read.text, 'lxml')
+#         pic_tag = soup.find('div', class_='bookimage').find('img')['src']
+#         full_image_url = urljoin(url_read, pic_tag)
+#         image_name_ext = (pic_tag.split('/'))[2]
+#         # imagename,ext = (image_name_ext.split('/'))
+#         splited_url = urlsplit(full_image_url)
+#         scheme, netloc, path, query, fragment = (splited_url)
+#         print(splited_url)
+#         filename = urlsplit(full_image_url).path.split('/')[-1]
+#         print(filename)
+#         save_pic(full_image_url, filename)
+#     except requests.exceptions.HTTPError:
+#         print('Страница не существует')
