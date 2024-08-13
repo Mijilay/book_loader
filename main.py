240149f8f6d7bf4adbabc5dff952a8d38ed31a9d
Mijilay <113235115+Mijilay@users.noreply.github.com>
2024-08-13 18:53:41 +0300

    fixed code

--- main.py
+++ main.py
@@ -3,16 +3,10 @@ import os
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 from urllib.parse import urljoin
-from urllib.parse import urlsplit, unquote
+from urllib.parse import urlsplit
 import argparse
 
 
-parser = argparse.ArgumentParser(description='Загрузка книг из бесплатной онлайн библиотеки. Выберите, сколько книг хотите скачать введя диопазон id.')
-parser.add_argument("--start_id", default=1, help='id первой книги')
-parser.add_argument("--end_id", default=10, help='id последней книги')
-args = parser.parse_args()
-arg_str_1 = int(args.start_id)
-arg_str_2 = int(args.end_id)
 def check_for_redirect(response):
     if response.history:
         raise requests.exceptions.HTTPError
@@ -42,27 +36,33 @@ def parse_book_page(response, url):
     genres_text = [genre.text for genre in genre_tag]
     title_tag = soup.find('h1').text
     title,author = title_tag.split('::')
-
     pic_tag = soup.find('div', class_='bookimage').find('img')['src']
     full_image_url = urljoin(url, pic_tag)
     book_parameters = {'title': title, 'author':author, 'genre': genres_text, 'comments': comments_text, 'image_url': full_image_url}
     return book_parameters
 
-for number in range(arg_str_1, arg_str_2):
-    try:  
-        url_text = f'https://tululu.org/txt.php?id={number}'
-        url_read= f'https://tululu.org/b{number}'
-        response_url_read = requests.get(url_read)
-        response_url_read.raise_for_status()
-        response_url_text = requests.get(url_text)
-        response_url_text.raise_for_status()
-        check_for_redirect(response_url_text)
-        title = parse_book_page(response_url_read, url_read)['title']
-        filename_book = f'{number}.{title.strip()}'
-        full_image_url = parse_book_page(response_url_read, url_read)['image_url']
-        save_book(url_text, filename_book)
-        save_pic(full_image_url)
 
-        
-    except requests.exceptions.HTTPError:
-        print('Страница не существует')
+def main():
+    parser = argparse.ArgumentParser(description='Загрузка книг из бесплатной онлайн библиотеки. Выберите, сколько книг хотите скачать введя диапазон id.')
+    parser.add_argument("--start_id", default=1, help='id первой книги', type=int)
+    parser.add_argument("--end_id", default=10, help='id последней книги', type=int)
+    args = parser.parse_args()
+
+    for number in range(args.start_id, args.end_id):
+        try:  
+            url_text = f'https://tululu.org/txt.php?id={number}'
+            url_read= f'https://tululu.org/b{number}'
+            response_url_read = requests.get(url_read)
+            response_url_read.raise_for_status()
+            response_url_text = requests.get(url_text)
+            response_url_text.raise_for_status()
+            check_for_redirect(response_url_text)
+            title = parse_book_page(response_url_read, url_read)['title']
+            filename_book = f'{number}.{title.strip()}'
+            full_image_url = parse_book_page(response_url_read, url_read)['image_url']
+            save_book(url_text, filename_book)
+            save_pic(full_image_url)
+        except requests.exceptions.HTTPError:
+            print('Страница не существует')
+if __name__=="__main__":
+    main()
