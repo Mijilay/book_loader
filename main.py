87137470e6ea9ae4c0b6fac1743f93b15e05bc91
Mijilay <113235115+Mijilay@users.noreply.github.com>
2024-08-20 18:12:54 +0300

    optimize code

--- main.py
+++ main.py
@@ -11,13 +11,15 @@ def check_for_redirect(response):
     if response.history:
         raise requests.exceptions.HTTPError
 
+
 def save_book(url, filename, folder='books/'):
     os.makedirs('books', exist_ok=True)
     response = requests.get(url)
     response.raise_for_status()
     filepath = os.path.join(f'{folder}{sanitize_filename(filename)}.txt')
-    with open (filepath, 'wb') as file:
+    with open(filepath, 'wb') as file:
         file.write(response.content)
+
 
 def save_pic(url, folder='covers/'):
     filename = urlsplit(url).path.split('/')[-1]
@@ -25,8 +27,10 @@ def save_pic(url, folder='covers/'):
     response = requests.get(url)
     response.raise_for_status()
     filepath = os.path.join(f'{folder}{sanitize_filename(filename)}')
-    with open (filepath, 'wb') as file:
+    with open(filepath, 'wb') as file:
         file.write(response.content)
+
+
 def parse_book_page(response, url):
     soup = BeautifulSoup(response.text, 'lxml')
     comment_tag = soup.find_all('div', class_='texts')
@@ -35,10 +39,10 @@ def parse_book_page(response, url):
     genre_tag = soup.find('span', class_='d_book').find_all('a')
     genres_text = [genre.text for genre in genre_tag]
     title_tag = soup.find('h1').text
-    title,author = title_tag.split('::')
+    title, author = title_tag.split('::')
     pic_tag = soup.find('div', class_='bookimage').find('img')['src']
     full_image_url = urljoin(url, pic_tag)
-    book_parameters = {'title': title, 'author':author, 'genre': genres_text, 'comments': comments_text, 'image_url': full_image_url}
+    book_parameters = {'title': title, 'author': author, 'genre': genres_text, 'comments': comments_text, 'image_url': full_image_url}
     return book_parameters
 
 
@@ -49,20 +53,24 @@ def main():
     args = parser.parse_args()
 
     for number in range(args.start_id, args.end_id):
-        try:  
+        try:
             url_text = f'https://tululu.org/txt.php?id={number}'
-            url_read= f'https://tululu.org/b{number}'
-            response_url_read = requests.get(url_read)
-            response_url_read.raise_for_status()
+            url = f'https://tululu.org/b{number}'
+            response_url = requests.get(url)
+            check_for_redirect(response_url)
+            response_url.raise_for_status()
             response_url_text = requests.get(url_text)
             response_url_text.raise_for_status()
             check_for_redirect(response_url_text)
-            title = parse_book_page(response_url_read, url_read)['title']
+            parse_book_url = parse_book_page(response_url, url)
+            title = parse_book_url['title']
             filename_book = f'{number}.{title.strip()}'
-            full_image_url = parse_book_page(response_url_read, url_read)['image_url']
+            full_image_url = parse_book_url['image_url']
             save_book(url_text, filename_book)
             save_pic(full_image_url)
         except requests.exceptions.HTTPError:
             print('Страница не существует')
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     main()
